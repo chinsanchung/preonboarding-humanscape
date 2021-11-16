ee485402be95aaa194dc7aaeea001420a2ebcf78
yhshin0 <asyh1047@naver.com>
2021-11-16 14:37:46 +0900

    chore: clinical controller enterData API 삭제
    - clinical 컨트롤러의 enterData() 삭제
    resolves: #4

--- src/clinical/clinical.controller.ts
+++ src/clinical/clinical.controller.ts
@@ -6,14 +6,6 @@ import { Clinical } from './entities/clinical.entity';
 export class ClinicalController {
   constructor(private readonly clinicalService: ClinicalService) {}
 
-  //테스트용도. 추후 삭제 할 것
-  @Get()
-  async enterData() {
-    const data = await this.clinicalService.enterInitialData();
-    return data;
-  }
-  ///////////
-
   @Get(':id')
   async findOneClinical(@Param('id') id: string): Promise<Clinical> {
     return this.clinicalService.findOneClinical(Number(id));
