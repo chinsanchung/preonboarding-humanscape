5470d969280a87b36c5c96fbfddb87fca4d7f80c
lhj0621 <lhj0621@gmail.com>
2021-11-16 10:56:59 +0900

    feat: 임상 리스트 / 검색
    - 임상 리스트 출력 & 검색 API 개발
    - date-fns 모듈 사용
    - query dto 생성
    
    resolves: #8 #9

--- src/clinical/clinical.controller.ts
+++ src/clinical/clinical.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ClinicalService } from './clinical.service';
+import { QueryDto } from './dto/Query.dto';
+import { Clinical } from './entities/clinical.entity';
 
 @Controller('clinical')
 export class ClinicalController {
   constructor(private readonly clinicalService: ClinicalService) {}
+
+  @Get()
+  getListClinical(
+    @Query() query: QueryDto,
+  ): Promise<{ data: Clinical[]; count: number }> {
+    return this.clinicalService.getListClinical(query);
+  }
 }
--- src/clinical/clinical.service.ts
+++ src/clinical/clinical.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ClinicalRepository } from './clinical.repository';
+import { QueryDto } from './dto/Query.dto';
+import { Clinical } from './entities/clinical.entity';
 
 @Injectable()
 export class ClinicalService {
@@ -8,4 +10,10 @@ export class ClinicalService {
     @InjectRepository(ClinicalRepository)
     private readonly clinicalRepository: ClinicalRepository,
   ) {}
+
+  async getListClinical(
+    query: QueryDto,
+  ): Promise<{ data: Clinical[]; count: number }> {
+    return await this.clinicalRepository.getListClinical(query);
+  }
 }
